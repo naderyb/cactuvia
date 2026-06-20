71d878ce1d93ffd36c5ca1736c773928dc4f3af2
youb nader <youb.nader@gmail.com>
2026-06-20 11:38:10 +0100

    [feat] - i added metadata cuz im bored

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,12 @@ import Rituels from "./components/sections/Rituels";
 import Journal from "./components/sections/Journal";
 import Footer from "./components/sections/Footer";
 
+//this is just a test page to render all the sections in one page for development purposes, it will be removed later
+export const metadata = {
+  title: "CACTUVIA - Soins Naturels · Algérie",
+  description:
+    "Des cosmétiques naturelles façonnées par la terre. Formules à base de gel de cactus et plantes pour révéler la beauté authentique de chaque femme.",
+};
 export default function Home() {
   return (
     <main>
